88b328ef28785b34c5e8d98d62fc4efe697029c6
BigBear <alanxiong@foxmail.com>
2024-09-14 11:36:15 +0800

    Update bloom.go
    用法过时了，已更新

--- bloom/bloom.go
+++ bloom/bloom.go
@@ -8,7 +8,10 @@ import (
 )
 
 func main() {
-	store := redis.New("localhost:6379")
+	//store := redis.New("192.168.8.6:6379") //过时的
+	redconf := redis.RedisConf{Host: "192.168.8.6:6379",
+	Pass: "redis_6X6RSk", Type: "node"}
+	store := redis.MustNewRedis(redconf)
 	filter := bloom.New(store, "testbloom", 64)
 	filter.Add([]byte("kevin"))
 	filter.Add([]byte("wan"))
